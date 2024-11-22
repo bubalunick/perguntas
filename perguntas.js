36e08befc0e05a0c545f1883198a878ea726dfb6
bubalunick <00001133095720sp@al.educacao.sp.gov.br>
2024-11-22 16:08:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,55 @@
+criaCartao(
+    'Geografia', 
+    'Qual a capital do japão?',
+    'Tóquio'
+)
+
+
+criaCartao(
+    'Biologia', 
+    'Quais são as principais questões da biologia?',
+    'Ecologia,botânica,bioenergética, fisiologica humana, citologia'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual o resultado de 40% de 109186?',
+    'O resultado é 43674.4'
+)
+
+criaCartao(
+    'História',
+    ' O feudalismo foi um sistema econômico, social e político na:',
+    ' Europa Ocidental durante a Idade Média, em especial na Baixa Idade Média'
+)
+
+criaCartao(
+    'Sociologia',
+    'A sociologia é uma ciência humana que estuda a sociedade. Das opções abaixo, a que não contempla um de seus objetivos é:?',
+    'Compreender a existência humana e o saber por meio da análise racional relacionada com a história.'
+)
+
+
+criaCartao(
+    'Filosofia',
+    'René Descartes desenvolveu um método filosófico baseado na dúvida para encontrar uma certeza na qual possa fundamentar o conhecimento seguro. Essa certeza fundamental de Descartes é chamada de cogito e sua formulação principal diz:',
+    ' "Penso, logo existo"'
+)
+
+criaCartao(
+    'cinema',
+    'Qual foi o primeiro longa-metragem de animação?',
+    'El Apóstol, (1917) do argentino Quirino Cristiani'
+)
+
+criaCartao(
+    'Fisiculturismo',
+    'para que serve o fisiculturimo?',
+    'Ganhar massa muscular'
+)
+
+criaCartao(
+    'Marvel',
+    'Qual é o personagem que é conhecido por ser um morcego?',
+    'Batman'
+)
